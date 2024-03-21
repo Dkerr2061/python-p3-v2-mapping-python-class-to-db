68f67fdd4eef3a01227363d40f3952c458f8ffbd
David Kerr <kerrky@gmail.com>
2024-03-21 17:02:01 -0400

    Complete Lab

--- lib/debug.py
+++ lib/debug.py
@@ -6,4 +6,15 @@ from department import Department
 
 import ipdb
 
+Department.drop_table()
+Department.create_table()
+
+payroll = Department("Payroll", "Building A, 5th Floor")
+payroll.save()
+
+hr = Department("Human Resources", "Building C, East Wing")
+hr.save()
+
+Department.create('Engineering', 'Building B, 2nd Floor')
+
 ipdb.set_trace()
